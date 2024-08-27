4900a326d638324ca00c1d71d2e1b93f2a385956
dtsudo <dtsudo@users.noreply.github.com>
2024-08-27 02:12:04 -0500

    awef

---
+++ sta-glitch-investigation/data/dataFiles/file6.js
@@ -0,0 +1,12 @@
+if (!window.files) window.files = {}; 
+window.files['contrib/extras/text.json'] = ` 
+{
+	"level" : {
+		"old-7" : "The Ocean Bridge OLD",
+		"old-6b" : "Branching path OLD",
+		"0-0B" : "Learning the slopes B-side",
+		"0-7B" : "The ocean bridge B-side"
+	}
+}`; 
+
+ window.superTuxAdvanceWebVersion.simulatedFileSystem.addFile('contrib/extras/text.json', ''); 
