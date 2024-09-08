d3713a907401b93d5cea71cf543e98bde07bdc9b
dtsudo <dtsudo@users.noreply.github.com>
2024-09-08 02:29:19 -0500

    awef

---
+++ sta-glitch-investigation/data/dataFiles/file3.js
@@ -0,0 +1,8 @@
+if (!window.files) window.files = {}; 
+window.files['contrib/extras/info.json'] = ` 
+{
+	"name" : "Extras",
+	"worldmap" : "extra.json"
+}`; 
+
+ window.superTuxAdvanceWebVersion.simulatedFileSystem.addFile('contrib/extras/info.json', ''); 
